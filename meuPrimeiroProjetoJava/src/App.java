bafad44f3f59facf1d348c5db2fc207e36549a2f
Gabriel Erick <gabrielmmve13@gmail.com>
2024-08-09 09:20:27 -0300

    first commit

---
+++ meuPrimeiroProjetoJava/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("um ola ao mais novo programador java");
+    };
+
+    public int soma(int valor1, int valor2){
+        return valor1 + valor2;
+    };
+
+    public void imprimir(){
+        System.out.println(soma(10,20));
+    };
+}
