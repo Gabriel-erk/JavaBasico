b2f689c3e1e80b01e4b32f514cf0380a027c6aeb
Gabriel Erick <gabrielmmve13@gmail.com>
2024-08-12 10:40:26 -0300

    métodos/excluindo arquivos

--- java-anatomia-classes/src/edu/gabriel/primeirasemana/Metodos.java
+++ java-anatomia-classes/src/edu/gabriel/primeirasemana/Metodos.java
@@ -1,13 +1,17 @@
-public class App {
+package edu.gabriel.primeirasemana;
+public class Metodos {
     public static void main(String[] args) throws Exception {
         System.out.println("um ola ao mais novo programador java");
+
+        System.out.println(soma(10,20));
+        imprimir();
     };
 
-    public int soma(int valor1, int valor2){
+    public static int soma(int valor1, int valor2){
         return valor1 + valor2;
     };
 
-    public void imprimir(){
+    public static void imprimir(){
         System.out.println(soma(10,20));
     };
 }
