976a8ef03de5a2e0fa6b03b9fb82b597da7faac1
Gabriel Erick <gabrielmmve13@gmail.com>
2024-08-14 16:30:18 -0300

    conhecendo comandos do terminal e geração automática de documentação

---
+++ java-doc/src/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+* logo abaixo é a forma de comentário para documentação  
+*/
+
+/** 
+* @author Gabriel Erick
+* @version 1.0
+* @since 14/01/24 
+*/
+
+public class Calculadora {
+    /** 
+     * este método retorna a soma de dois numeros
+     * @param numeroUm - este é o primeiro parâmetro
+     * @param numeroDois - este é o segundo parâmetro
+     * documentando de forma especializada, ao passar o mouse sobre o nome do método, mostrará o nome, dunção do método a classe pai, e a descrição dos parâmetros
+    */
+    public int soma(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ java-terminal/src/ComandoTerminal.java
@@ -0,0 +1,10 @@
+/**
+ * ComandoTerminal
+ */
+public class ComandoTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi tropa, fui executado pelo terminal");
+    }
+    
+
+}
